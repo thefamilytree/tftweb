a2015f54224bd630055adeec446c96fde81d387d
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-12 23:48:31 +0530

    Refactor the team data & component

---
+++ src/data/TeamData.js
@@ -0,0 +1,62 @@
+const TeamData = [
+  {
+    name: 'Nupur Paiva',
+    image: '/assets/Team/nupur.jpg',
+    shortText:
+      'Nupur D. Paiva is a Chartered Clinical Psychologist, Associate Fellow with the British Psychological Society, Child Psychotherapist and mother-of-two, all of which she has worked very hard at. Especially, the two children.',
+    fullText: `Nupur D. Paiva is a Chartered Clinical Psychologist, Associate Fellow with the British Psychological Society, Child Psychotherapist and mother-of-two, all of which she has  worked very hard at. Especially, the two children.
+        <br />
+        She started Family Tree: Child and Adolescent Mental Health team, in 2018 in New Delhi. She taught 'Infant Observation' on the Psychotherapy training course at Ambedkar University, Delhi from 2012-2020. She is regularly invited to write articles on emotional and mental health for national dailies and for discussions on conference panels. You can find a lot of this through a google search.
+        <br />
+        In 2014 she decided to train further, especially on how to work with hard-to-engage-adolescents, anxiety and depression in young people. She completed a certification in Intensive Short Term Dynamic Psychotherapy. The process of this training took five years of consistent truth-facing and changed her life, making her more courageous. Now she is training others to work with this technique. She is the author of Love & Rage: The Inner Worlds of Children, which was published in 2017, which is very close to her heart and has a lot of detail on what psychotherapy with children looks like. Along with her husband, Richard Paiva, she is a co-founder of The Art of Sport – a development programme for girls, using sport and group therapy.
+        <br />
+        For her own energy and emotional balance, she likes to dance and though her motor coordination does not approve, she does not let that stop her.`,
+  },
+  {
+    name: 'Neha Gupta',
+    image: '/assets/Team/neha.jpg',
+    shortText:
+      'The first to join in 2018 was Neha Gupta when Family Tree was a wee tentative experiment. It was with her that we became a team. She and I. Super well-trained, Neha has an MSc Child & Adolescent Mental Health and Psychological Practice from The University of Edinburgh, UK and a BA in Psychology (Honours) from Lady Shri Ram College, Delhi University.',
+    fullText: `The first to join in 2018 was Neha Gupta when Family Tree was a wee tentative experiment. It was with her that we became a team. She and I. Super well-trained, Neha has an MSc Child & Adolescent Mental Health and Psychological Practice from The University of Edinburgh, UK and a BA in Psychology (Honours) from Lady Shri Ram College, Delhi University.
+        <br />
+        More than the fact that she is psychoanalytically trained at PTRC (Psychoanalytic Training and Research Centre), Mumbai, it is her natural empathy toward the emotional world of young humans that makes her an effective child psychotherapist.
+        <br />
+        She has worked with children and young adults for over 7 years in a variety of settings. She has also worked with children with special developmental needs.
+        <br />
+        Neha facilitates a group of girls between the ages 10-13 years at @artofsportindia too.
+        <br />
+        Neha's first true love has always been animals. She's a dog lover and is happy to spend the rest of her life being a Dog mom.`,
+  },
+  {
+    name: 'Ambika Singh',
+    image: '/assets/Team/ambika.jpg',
+    shortText:
+      'Ambika joined Family Tree in February 2020 and has valiantly worked online with adolescents throughout this lockdown. Ambika Singh is a trained Psychoanalytic Psychotherapist having completed her MPhil from Ambedkar University Delhi; Masters in Psychosocial Clinical Studies from Ambedkar University, Delhi; and a Bachelors in Psychology (Honours) from Lady Shri Ram College, Delhi University.',
+    fullText: `Ambika joined Family Tree in February 2020 and has valiantly worked online with adolescents throughout this lockdown. Ambika Singh is a trained Psychoanalytic Psychotherapist having completed her MPhil from Ambedkar University Delhi; Masters in Psychosocial Clinical Studies from Ambedkar University, Delhi; and a Bachelors in Psychology (Honours) from Lady Shri Ram College, Delhi University.
+        <br />
+        She has worked in places few dare to tread - mental health centres in university settings. She has borne the red-tape, the senior administration who don't understand the emotional issues of young people, the high rates of self harm and suicidal thoughts in her clients.
+        <br />
+        At Family Tree we are grateful to Ambika for her experience and commitment to working on the darkest of real subjects - understanding 'suicidality' and its representations in the Indian context.
+        <br />
+        She is occasionally frightened of working with small kids though - and we are helping her get over that.
+        <br />
+        When she isn't working, she can be found dancing to 90s music or talking to her plants.`,
+  },
+  {
+    name: 'Shweta Dharamdasani',
+    image: '/assets/Team/shweta.jpg',
+    shortText:
+      'Shweta Dharamdasani is a psychoanalytically oriented psychotherapist with an MPhil from Ambedkar University, Delhi. She is a researcher and writer, committed to depth thinking and learning. Her area of interest is in how culture and psyche influence each other.',
+    fullText: `Shweta Dharamdasani is a psychoanalytically oriented psychotherapist with an MPhil from Ambedkar University, Delhi. She is a researcher and writer, committed to depth thinking and learning. Her area of interest is in how culture and psyche influence each other.
+        <br />
+        Shweta has also been trained at Antarnaad foundation, Ahmadabad for a year and is a RCI certified Rehabilitation Psychologist, all of which makes her a highly prized clinician.
+        <br />
+        Shweta finds it very easy to connect with children and happily joins in the fitness group activities while she facilitates a group at @artofsportindia which she is missing terribly during the lockdown even though it is now online.
+        <br />
+        Recently she has also been working towards building a vocabulary for doing psychoanalysis in Hindi - an attempt to take psychoanalysis beyond the clinic and the elite, thereby making it more accessible.
+        <br />
+        Much like the children she works with through play and stories, Shweta enjoys craft activities-making things out of waste material and scrap. What we appreciate the most though is what she makes out of the distress, anxiety and silence that she works with in her young clients.`,
+  },
+];
+
+export { TeamData };
--- src/scripts/index-script.js
+++ src/scripts/index-script.js
@@ -1,167 +1,171 @@
 //NAVBAR
 
-const navbar = document.querySelector("nav");
-const header = document.querySelector("header");
-const logoImg = document.querySelector(".logo-img")
+const navbar = document.querySelector('nav');
+const header = document.querySelector('header');
+const logoImg = document.querySelector('.logo-img');
 
 const headerOptions = {
-    rootMargin: "-100px 0px 0px 0px"
+  rootMargin: '-100px 0px 0px 0px',
 };
 
-const headerObserver = new IntersectionObserver(function(
-    entries,
-    headerObserver
-){
-    entries.forEach(entry => {
-        //console.log(entry.target)
-        if(!entry.isIntersecting){
-            navbar.classList.add("navbar-scrolled")
-            logoImg.src = "/assets/logo.png";
-        } else {
-            navbar.classList.remove("navbar-scrolled")
-            logoImg.src = "/assets/logo-white.png";
-        }
-    });
-}, 
-headerOptions);
+const headerObserver = new IntersectionObserver(function (entries, headerObserver) {
+  entries.forEach(entry => {
+    //console.log(entry.target)
+    if (!entry.isIntersecting) {
+      navbar.classList.add('navbar-scrolled');
+      logoImg.src = '/assets/logo.png';
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+      logoImg.src = '/assets/logo-white.png';
+    }
+  });
+}, headerOptions);
 
 headerObserver.observe(header);
 
 //Mobile Navbar
 
-const mobileNavbar = document.querySelector(".navbar");
-const mobileLogoImg = document.querySelector(".mobile-logo-img")
-const mobileHeaderObserver = new IntersectionObserver(function(
-    entries,
-    mobileHeaderObserver
-){
-    entries.forEach(entry => {
-        //console.log(entry.target)
-        if(!entry.isIntersecting){
-            mobileNavbar.classList.add("navbar-mobile-scrolled")
-            mobileLogoImg.src = "/assets/logo.png";
-        } else {
-            mobileNavbar.classList.remove("navbar-mobile-scrolled")
-            mobileLogoImg.src = "/assets/logo-white.png";
-        }
-    });
-}, 
-headerOptions);
+const mobileNavbar = document.querySelector('.navbar');
+const mobileLogoImg = document.querySelector('.mobile-logo-img');
+const mobileHeaderObserver = new IntersectionObserver(function (entries, mobileHeaderObserver) {
+  entries.forEach(entry => {
+    //console.log(entry.target)
+    if (!entry.isIntersecting) {
+      mobileNavbar.classList.add('navbar-mobile-scrolled');
+      mobileLogoImg.src = '/assets/logo.png';
+    } else {
+      mobileNavbar.classList.remove('navbar-mobile-scrolled');
+      mobileLogoImg.src = '/assets/logo-white.png';
+    }
+  });
+}, headerOptions);
 
 mobileHeaderObserver.observe(header);
 
 //Sidebar
 
-const menuIcon = document.querySelector(".menu-icon")
-const sideBar = document.querySelector("#sidebar")
-const closeIcon = document.querySelector(".close")
-
+const menuIcon = document.querySelector('.menu-icon');
+const sideBar = document.querySelector('#sidebar');
+const closeIcon = document.querySelector('.close');
 
 menuIcon.addEventListener('click', () => {
-    if(sideBar.style.width == "0%"){
-        sideBar.style.width = "100%";
-        sideBar.style.opacity = "1"
-    } else {
-        sideBar.style.opacity = "0"
-    }
-})
+  if (sideBar.style.width == '0%') {
+    sideBar.style.width = '100%';
+    sideBar.style.opacity = '1';
+  } else {
+    sideBar.style.opacity = '0';
+  }
+});
 
 closeIcon.addEventListener('click', () => {
-    if(sideBar.style.width == "100%"){
-        sideBar.style.width = "0%";
-        sideBar.style.opacity = "0"
+  if (sideBar.style.width == '100%') {
+    sideBar.style.width = '0%';
+    sideBar.style.opacity = '0';
+  } else {
+    sideBar.style.width = '100%';
+    sideBar.style.opacity = '1';
+  }
+});
+
+const mobileLinks = document.getElementsByClassName('mobile-link');
+
+for (var i = 0; i < mobileLinks.length; i++) {
+  mobileLinks[i].addEventListener('click', () => {
+    if (sideBar.style.width == '0%') {
+      sideBar.style.width = '100%';
+      sideBar.style.opacity = '1';
     } else {
-        sideBar.style.width = "100%";
-        sideBar.style.opacity = "1"
+      sideBar.style.opacity = '0';
     }
-})
-
-const mobileLinks = document.getElementsByClassName("mobile-link")
-
-for(var i=0; i<mobileLinks.length; i++){
-    mobileLinks[i].addEventListener('click', () => {
-        if(sideBar.style.width == "0%"){
-            sideBar.style.width = "100%";
-            sideBar.style.opacity = "1"
-        } else {
-            sideBar.style.opacity = "0"
-        }
-    })
+  });
 }
 
 //TEAM
 
 //Nupur
-const nupurShortText = document.querySelector(".nupur-short-text")
-const nupurtFullText = document.querySelector(".nupur-full-text")
-const nupurBtn = document.querySelector(".nupur-btn")
-    
-nupurBtn.addEventListener('click', () => {
-    if (nupurShortText.style.display == 'block'){
-        nupurShortText.style.display = 'none'
-        nupurtFullText.style.display = 'block'
-        nupurBtn.innerHTML = "Read Less"
+// const nupurShortText = document.querySelector('.nupur-short-text');
+// const nupurtFullText = document.querySelector('.nupur-full-text');
+// const nupurBtn = document.querySelector('.nupur-btn');
+
+// nupurBtn.addEventListener('click', () => {
+//   if (nupurShortText.style.display == 'block') {
+//     nupurShortText.style.display = 'none';
+//     nupurtFullText.style.display = 'block';
+//     nupurBtn.innerHTML = 'Read Less';
+//   } else {
+//     nupurShortText.style.display = 'block';
+//     nupurtFullText.style.display = 'none';
+//     nupurBtn.innerHTML = 'Read More';
+//   }
+// });
+
+// //Neha
+// const nehaShortText = document.querySelector('.neha-short-text');
+// const nehaFullText = document.querySelector('.neha-full-text');
+// const nehaBtn = document.querySelector('.neha-btn');
+
+// nehaBtn.addEventListener('click', () => {
+//   if (nehaShortText.style.display == 'block') {
+//     nehaShortText.style.display = 'none';
+//     nehaFullText.style.display = 'block';
+//     nehaBtn.innerHTML = 'Read Less';
+//   } else {
+//     nehaShortText.style.display = 'block';
+//     nehaFullText.style.display = 'none';
+//     nehaBtn.innerHTML = 'Read More';
+//   }
+// });
+
+// //Ambika
+// const ambikaShortText = document.querySelector('.ambika-short-text');
+// const ambikaFullText = document.querySelector('.ambika-full-text');
+// const ambikaBtn = document.querySelector('.ambika-btn');
+
+// ambikaBtn.addEventListener('click', () => {
+//   if (ambikaShortText.style.display == 'block') {
+//     ambikaShortText.style.display = 'none';
+//     ambikaFullText.style.display = 'block';
+//     ambikaBtn.innerHTML = 'Read Less';
+//   } else {
+//     ambikaShortText.style.display = 'block';
+//     ambikaFullText.style.display = 'none';
+//     ambikaBtn.innerHTML = 'Read More';
+//   }
+// });
+
+// //Shweta
+// const shwetaShortText = document.querySelector('.shweta-short-text');
+// const shwetaFullText = document.querySelector('.shweta-full-text');
+// const shwetaBtn = document.querySelector('.shweta-btn');
+
+// shwetaBtn.addEventListener('click', () => {
+//   if (shwetaShortText.style.display == 'block') {
+//     shwetaShortText.style.display = 'none';
+//     shwetaFullText.style.display = 'block';
+//     shwetaBtn.innerHTML = 'Read Less';
+//   } else {
+//     shwetaShortText.style.display = 'block';
+//     shwetaFullText.style.display = 'none';
+//     shwetaBtn.innerHTML = 'Read More';
+//   }
+// });
+
+const members = document.querySelectorAll('.member');
+
+members.forEach(member => {
+  const shortText = member.querySelector('.short-text');
+  const fullText = member.querySelector('.full-text');
+  const btn = member.querySelector('.btn-outline');
+
+  btn.addEventListener('click', () => {
+    if (shortText.style.display === 'block') {
+      shortText.style.display = 'none';
+      fullText.style.display = 'block';
+      btn.innerHTML = 'Read Less';
+    } else {
+      shortText.style.display = 'block';
+      fullText.style.display = 'none';
+      btn.innerHTML = 'Read More';
     }
-    else{
-        nupurShortText.style.display = 'block'
-        nupurtFullText.style.display = 'none'
-        nupurBtn.innerHTML = "Read More"
-    }
-})
-
-//Neha
-const nehaShortText = document.querySelector(".neha-short-text")
-const nehaFullText = document.querySelector(".neha-full-text")
-const nehaBtn = document.querySelector(".neha-btn")
-
-nehaBtn.addEventListener('click', () => {
-    if (nehaShortText.style.display == 'block'){
-        nehaShortText.style.display = 'none'
-        nehaFullText.style.display = 'block'
-        nehaBtn.innerHTML = "Read Less"
-    }
-    else{
-        nehaShortText.style.display = 'block'
-        nehaFullText.style.display = 'none'
-        nehaBtn.innerHTML = "Read More"
-    }
-})
-
-//Ambika
-const ambikaShortText = document.querySelector(".ambika-short-text")
-const ambikaFullText = document.querySelector(".ambika-full-text")
-const ambikaBtn = document.querySelector(".ambika-btn")
-
-ambikaBtn.addEventListener('click', () => {
-    if (ambikaShortText.style.display == 'block'){
-        ambikaShortText.style.display = 'none'
-        ambikaFullText.style.display = 'block'
-        ambikaBtn.innerHTML = "Read Less"
-    }
-    else{
-        ambikaShortText.style.display = 'block'
-        ambikaFullText.style.display = 'none'
-        ambikaBtn.innerHTML = "Read More"
-    }
-})
-
-//Shweta
-const shwetaShortText = document.querySelector(".shweta-short-text")
-const shwetaFullText = document.querySelector(".shweta-full-text")
-const shwetaBtn = document.querySelector(".shweta-btn")
-
-shwetaBtn.addEventListener('click', () => {
-    if (shwetaShortText.style.display == 'block'){
-        shwetaShortText.style.display = 'none'
-        shwetaFullText.style.display = 'block'
-        shwetaBtn.innerHTML = "Read Less"
-    }
-    else{
-        shwetaShortText.style.display = 'block'
-        shwetaFullText.style.display = 'none'
-        shwetaBtn.innerHTML = "Read More"
-    }
-})
-
-
-
+  });
+});
